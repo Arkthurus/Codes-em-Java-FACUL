e04e271a2f8df4e2fe52074ec38e1c6edd86a71b
Arthur PC Casa <ArthuroJPC@Gmail.com>
2025-04-11 16:30:31 -0300

    Associação de Classes Exemplo Prof Modificação FINALIZADA

--- AssociacaoDeClasses/src/ExemploProf2MinhaTentaviaMenu.java
+++ AssociacaoDeClasses/src/ExemploProf2MinhaTentaviaMenu.java
@@ -3,14 +3,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ExemploProf2MinhaTentaviaMenu{
-   static int numPedido=0;
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         ArrayList <Cliente> clientes = new ArrayList<>();
 
-        ArrayList <Pedido> pedidos = new ArrayList<>();
 
         while (true){
             System.out.printf("+-----------Clientes----------+\n");
@@ -24,30 +23,37 @@ public class ExemploProf2MinhaTentaviaMenu{
             System.out.printf("+-----------------------------+\n");
             int escolha= sc.nextInt();
             for (int i = 0; i < clientes.size(); i++) {
-                if (escolha==(i+2)){MenuCliente(clientes.get(i), pedidos);}
+                if (escolha==(i+2)){MenuCliente(clientes.get(i));}
             }
             if (escolha==1){addCliente(clientes);}
         }
     }
-    protected static void newPedido(Cliente cliente, ArrayList<Pedido> pedidos){
+    protected static void newPedido(Cliente cliente){
 
         Scanner sc = new Scanner(System.in);
 
-
-        while(true){
-            pedidos.add(new Pedido(numPedido, cliente));
+        System.out.printf("Deseja  cadastrar um produto? [S/N]: ");
+        String opcao = sc.nextLine();
+        if (opcao.equalsIgnoreCase("N")) {return;}
+        cliente.pedidos.add(new Pedido(cliente.pedidos.size(), cliente));
+        while (true) {
             System.out.printf("Nome do produto: ");
-            String nomeP= sc.nextLine();
+            String nomeP = sc.nextLine();
             System.out.printf("Quantida comprada: ");
             int QTD = sc.nextInt();
             System.out.printf("Preço unitario: ");
-            double precoUni= sc.nextDouble();
-            pedidos.get(numPedido).itens.add(new ItemPedido( nomeP, QTD, precoUni));
+            double precoUni = sc.nextDouble();
+            cliente.pedidos.get(cliente.pedidos.size() - 1).itens.add(new ItemPedido(nomeP, QTD, precoUni));
             System.out.printf("Deseja adicionar mais um produto?\n");
             System.out.printf("[1]Sim [2]Não\n");
             int escolha = sc.nextInt();
-            if (escolha==1){numPedido++;sc.nextLine();continue;};
-            if (escolha==2){numPedido++;break;};
+            if (escolha == 1) {
+                sc.nextLine();
+                continue;
+            }
+            if (escolha == 2) {
+                break;
+            }
 
         }
     }
@@ -65,7 +71,7 @@ public class ExemploProf2MinhaTentaviaMenu{
         clientes.add(new Cliente( nome, email, cep));
     }
 
-    protected static void MenuCliente(Cliente cliente, ArrayList<Pedido> pedidos){
+    protected static void MenuCliente(Cliente cliente){
 
         Scanner sc = new Scanner(System.in);
 
@@ -83,10 +89,41 @@ public class ExemploProf2MinhaTentaviaMenu{
             System.out.printf("|                             |\n");
             System.out.printf("+-----------------------------+\n");
             int escolha = sc.nextInt();
-            for (int i = 0; i < 3; i++) {
-                if (escolha==i+1){newPedido(cliente, pedidos);}
-            }
+            if (escolha==1){newPedido(cliente);}
+            if (escolha==2){
 
+                if (cliente.pedidos.isEmpty()){System.out.printf("Nenhum Pedido atrelado ao Cliente!\n");continue;}
+
+                System.out.println("----------------------------------------------------");
+                for (int i = 0; i < cliente.pedidos.size(); i++) {
+
+                    System.out.println("Numero do pedido: " + (i+1));
+                    System.out.println("Cliente: " + cliente.getNome());
+                    System.out.println("Email: " + cliente.getEmail());
+                    System.out.println("Endereco: " + cliente.getEndereco());
+                    System.out.println();
+                    System.out.println("Itens do pedido:");
+                    System.out.println();
+
+                    /* Imprimindo os itens do pedido e calculando o total */
+                    double totalPedido = 0;
+
+                    for (ItemPedido item : cliente.pedidos.get(i).itens) {
+                        System.out.println("Produto: " + item.nome);
+                        System.out.println("Quantidade: " + item.quantidade);
+                        System.out.println("Preco unitario: R$ " + item.precoUnitario);
+                        System.out.println();
+
+                        totalPedido += item.quantidade * item.precoUnitario;
+                    }
+
+                    System.out.printf("Valor total do pedido: R$ %.2f\n", totalPedido);
+                    System.out.println("----------------------------------------------------");
+                    System.out.printf("[1]voltar");
+                    int escolha2 = sc.nextInt();
+                }
+            }
+            if (escolha==3){break;}
         }
     }
 }
@@ -95,6 +132,7 @@ class Cliente {
     private String nome;
     private String email;
     private String endereco;
+    public ArrayList<Pedido> pedidos = new ArrayList<>();
 
     /* Construtor da classe */
     public Cliente(String nome, String email, String endereco) {
@@ -115,6 +153,8 @@ class Cliente {
     public String getEndereco(){
         return this.endereco;
     }
+
+
 }
 
 class Pedido {
@@ -128,32 +168,6 @@ class Pedido {
         this.cliente = cliente;
     }
 
-    /* Imprimindo o pedido */
-    public void imprimirDetalhes() {
-
-        System.out.println("Numero do pedido: " + numero);
-        System.out.println("Cliente: " + cliente.getNome());
-        System.out.println("Email: " + cliente.getEmail());
-        System.out.println("Endereco: " + cliente.getEndereco());
-        System.out.println();
-        System.out.println("Itens do pedido:");
-        System.out.println();
-
-        /* Imprimindo os itens do pedido e calculando o total */
-        double totalPedido = 0;
-
-        for (ItemPedido item : itens) {
-            System.out.println("Produto: " + item.nome);
-            System.out.println("Quantidade: " + item.quantidade);
-            System.out.println("Preco unitario: R$ " + item.precoUnitario);
-            System.out.println();
-
-            totalPedido += item.quantidade * item.precoUnitario;
-        }
-
-        System.out.printf("Valor total do pedido: R$ %.2f", totalPedido);
-        System.out.println();
-    }
 }
 
 class ItemPedido {
